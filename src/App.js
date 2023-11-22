3ea193d26d0d6a56b90a50ccc45affeb736ea5d0
TusharRB <tusharbotare17@gmail.com>
2023-11-22 16:51:27 +0530

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,92 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm';
+import Alert from './Components/Alert';
+import About from './Components/About';
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+  // Routes
+} from "react-router-dom";
 
 function App() {
+
+  const [mode, setMode] = useState('light'); // whether dark mode is enable or not
+  const [alert, setAlert] = useState(null)
+
+  const showAlert = (message, type) => {
+
+    setAlert({
+      msg: message,
+      type: type
+    })
+
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500)
+
+
+
+  }
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark')
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enable", "success");
+      document.title = "TextUtils - Home (D Mode)"
+
+      // setInterval(() => {
+      //   document.title = "Install TextUtils Now"
+      // },1000)
+
+      // setInterval(() => {
+      //   document.title = "TextUtils Now"
+      // },2000)
+
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'Lavender';
+      showAlert("Light mode has been enable", "success");
+      document.title = "TextUtils - Home"
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Router>
+      <>
+
+        <Navbar title="TextUtils" aboutText="aboutText" mode={mode} toggleMode={toggleMode} />
+
+        <Alert alert={alert} />
+
+        <div className="container my-3">
+
+          <Switch>
+            <Route exact path='/about'>
+              <About />
+            </Route>
+
+            <Route exact path='/'>
+
+              <TextForm heading="Enter to text analyze below" 
+              showAlert={showAlert} mode={mode} />
+
+            </Route>
+          </Switch>
+
+
+          {/* <About /> */}
+        </div>
+
+      </>
+    </Router>
+
+
   );
 }
 
